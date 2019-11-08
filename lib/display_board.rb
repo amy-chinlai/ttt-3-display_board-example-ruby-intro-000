097faa08f28047ce0e1ad95ad6ffcd932b0a433c
Amy Chin-Lai <amy.chinlai@gmail.com>
2019-11-08 22:14:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,4 +3,5 @@ def display_board
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
- puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+ puts "   |   |   "
+ end
